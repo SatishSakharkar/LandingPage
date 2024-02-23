a0d7485f8e0df6f89fe566acb0161b4083d71e5a
SatishSakharkar <115470010+SatishSakharkar@users.noreply.github.com>
2024-02-23 11:28:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirectToPurchase() {
+    // You can customize this function to handle the redirect to the purchase page
+    alert("Redirecting to the purchase page!");
+    // window.location.href = "purchase.html"; // Uncomment and replace with the actual purchase page URL
+  }
+  
